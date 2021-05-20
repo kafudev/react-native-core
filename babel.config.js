e647b11cb2852b95078fe22a6748c7c88e8804cd
zpd106 <zpd106@sina.com>
2021-05-20 18:06:04 +0800

    chore: react-native 版本0.64以上

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  plugins: [['@babel/plugin-proposal-decorators', {legacy: true}]],
+  plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]],
   presets: ['module:metro-react-native-babel-preset'],
-}
+};
